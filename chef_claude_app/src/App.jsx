23811cc4acd8e4d380c8ce7cdbdd6e5dad838841
chauhanusha <ushaschauhan8@gmail.com>
2026-02-21 23:15:16 +0530

    getting recipe from ai

--- chef_claude_app/src/App.jsx
+++ chef_claude_app/src/App.jsx
@@ -1,5 +1,13 @@
+import Header from "./components/Header"
+import MainCon from "./components/MainCon"
+
 function App() {
-  
+  return (
+    <>
+    <Header/>
+    <MainCon/>
+    </>
+  )
 }
 
 export default App
